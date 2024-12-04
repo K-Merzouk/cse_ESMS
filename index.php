714edb97f65fcdf76f1a71a0d6a444272bdcacc7
K-Merzouk <ok_merzouk@esi.dz>
2024-12-05 00:12:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("head/meta.php");?>
+    <title>Accueil</title>
+    <?php require_once("head/link.php");?>
+
+
+
+</head>
+
+<body >
+<header><?php include 'includes/navbar.php';?></header>
+<body>
+    <section class="top">
+
+    </section>
+</body>
+
+    
+    <?php require_once("footer/footer.php");?>
+
+</html>
+
