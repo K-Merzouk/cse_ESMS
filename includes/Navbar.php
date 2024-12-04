c8beb5878f408539627338c72a449127637201dd
K-Merzouk <ok_merzouk@esi.dz>
2024-12-05 00:24:50 +0100

    Update Navbar.php

--- includes/Navbar.php
+++ includes/Navbar.php
@@ -8,14 +8,14 @@
                         <img src='../img/dwldbtn.png' class="Menu">
                 <div class="nav-link">
                         <div class='btn'> </div><ul>
-                        <li><a href="../index.php" class="A"><p>Home</p></a></li>
-                        <li><a href="../Articles/Articles.php" class="Ar">About</a></li>
-                        <li><a  href="../Inscription.php" class="I">Agenda</a></li>
-                        <li><a href="../Connexion.php" class= "C">Contact Us</a></li>
+                        <li><a href="../Home.php" class="A"><p>Home</p></a></li>
+                        <li><a href="../About.php" class="Ar">About</a></li>
+                        <li><a  href="../Agenda.php" class="I">Agenda</a></li>
+                        <li><a href="../Contact.php" class= "C">Contact Us</a></li>
                       
                     
                         </ul></div>
-                        <div class= "co"><li><a href="../Connexion.php" class= "">Register Now !</a></li></div>
+                        <div class= "co"><li><a href="../Inscription.php" class= "">Register Now !</a></li></div>
                        
                 
                         
