9a53f0740587e691e9bcfe40b78ea314ee5c3d95
K-Merzouk <ok_merzouk@esi.dz>
2024-12-05 23:27:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -33,19 +33,40 @@
         Dive into an <span class="yellow-highlight">immersive </span>  adventure with "<span class="green-highlight">Escape the Virus Lab</span>"! Team up to tackle exciting challenges in data analysis, cybersecurity, and artificial intelligence as you race to contain a simulated <span class="red-highlight">epidemic</span>. Are you clever enough to escape in time? A <span class= "green-highlight">one-of-a-kind </span> experience blending technology, science, and teamwork
         </p>
         </div>
-        </section class = "time and place">
-        <section> <div class = "time-place">Where? When?</div>
-    <br>
-    <br>
-    <div class = "time-place-details">
-    15/05/2025
-    <br>
-    ESI,Algiers
+        </section>
+    <section class="event-agenda">
+    <!-- Header Section-->
+    <div class="header">
+      <h1>Event Agenda</h1> <!-- Title -->
     </div>
-    <img src = "img/position.svg" , alt = "position" , class = "position-image">
- 
 
-</section>
+    <!-- Agenda Items -->
+    <div class="agenda"id="agenda">
+      <!-- First Agenda Item -->
+      <div class="agenda-item">
+        <div class="time1"> 9:15–10:30</div> <!-- Timeline -->
+        <div class="activity"> Welcome & briefing</div> <!-- Activity -->
+      </div>
+
+      <!-- Second Agenda Item -->
+      <div class="agenda-item">
+        <div class="time">11:00–12:30</div> <!-- Timeline -->
+        <div class="activity">Escape room session</div> <!-- Activity -->
+      </div>
+
+      <!-- Third Agenda Item -->
+      <div class="agenda-item">
+        <div class="time">12:30–13:30</div> <!-- Timeline -->
+        <div class="activity">Lunch break</div> <!-- Activity -->
+      </div>
+
+      <!-- Fourth Agenda Item -->
+      <div class="agenda-item">
+        <div class="time">13:30–14:30</div> <!-- Timeline -->
+        <div class="activity">Results & prizes</div> <!-- Activity -->
+      </div>
+    </div>
+  </section>
 </body>
 
     
