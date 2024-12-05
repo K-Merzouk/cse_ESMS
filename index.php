bb8c9cd8feb7317be53aeac22ec009af2ac0e54b
K-Merzouk <ok_merzouk@esi.dz>
2024-12-05 17:36:43 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -15,6 +15,13 @@
 <header><?php include 'includes/navbar.php';?></header>
 <body>
     <section class="top">
+        <div class= "top1"><div class ='welcome'>   <h1 class = "">  &nbsp;&nbsp;Escape The  </h1><p class= 'green'>Lab</p>  <br></div>
+        <br>
+        <br>
+       
+        <div class="welcomeS yellow"><h4>Solve, Survive, Escape</h4>
+        <div class="welcomeSI "><h4>The Future is in Your Hands!</h4>
+
 
     </section>
 </body>
